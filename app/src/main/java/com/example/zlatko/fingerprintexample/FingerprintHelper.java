18e0e814efee178e05931ce7e60da261d2801e21
Crutchcorn <crutchcorn@gmail.com>
2019-03-16 01:03:43 -0700

    Update deps

--- app/src/main/java/com/example/zlatko/fingerprintexample/FingerprintHelper.java
+++ app/src/main/java/com/example/zlatko/fingerprintexample/FingerprintHelper.java
@@ -1,44 +1,11 @@
 package com.example.zlatko.fingerprintexample;
 
-
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.Application;
-import android.app.KeyguardManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Build;
-import android.security.keystore.KeyGenParameterSpec;
-import android.security.keystore.KeyProperties;
-import android.security.keystore.UserNotAuthenticatedException;
-import android.util.Base64;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.cert.CertificateException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
 
 /**
  * Helper class for working with the fingerprint.
  */
 public class FingerprintHelper {
-
-
-
   private final Activity context;
 
   public FingerprintHelper(Activity context) {
